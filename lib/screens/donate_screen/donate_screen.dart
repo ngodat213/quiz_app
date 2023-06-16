c4351192cb0795c0dc15fbc42d41958df423003f
Hydra Coder <96688421+ngodat213@users.noreply.github.com>
2023-06-16 21:15:55 +0700

    update image

---
+++ lib/screens/donate_screen/donate_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DonateScreen extends StatelessWidget {
+  const DonateScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width: 64,
+              height: 64,
+child: ,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
